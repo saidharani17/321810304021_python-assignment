e5b4b9eb96ef3b8ef5ed00edaab5028f388b42ce
saidharani17 <67104227+saidharani17@users.noreply.github.com>
2020-06-19 09:24:34 +0530

    Add files via upload

---
+++ python_assignment_1.py
@@ -0,0 +1,93 @@
+  #  python_assignment_1
+
+  #  J sai dharani  -  321810304021
+
+
+  #  1. to design Simple calculator
+  x,y=10,10                #Addition
+  print(x+y)
+
+  x=15
+  y=15
+  z=x+y
+  print(z)
+
+  x,y=20,10                 #Subtraction
+  print(x-y)
+
+  x,y=5,3                      #Multiplication
+  print(x*y)
+
+  x,y=60,30                  #Division
+  print(x/y)
+
+  x,y=100,15                 #Modulus
+  print(x%y)
+
+  x=4**4                        #Exponentiation
+  print(x)
+
+  x,y=60,30                    #Floor Division
+  print(x//y)
+
+
+  #  2. Simple Interest(si)
+  p=150
+  t=14
+  r=3
+  si=p*t*r/100
+ print(si)
+
+
+  # 3. Area of a circle
+  PI=3.14
+  r=5
+ Area=PI*r*r
+ print(Area)
+
+
+  # 4. Area of triangle
+  a=10
+  b=4
+ Area=(a*b/2)
+ print(Area)
+
+
+  # 5. Temperature in celsiusvto farenheit
+  celsius=22.5
+  farenheit=(celsius*1.8)+32
+  print(farenheit)
+
+
+  # 6. Area of Rectangle
+  W=5
+  L=8
+ Area=W*L
+ print(Area)
+
+
+  # 7. Perimeter of a Square
+  a=20
+  P=4*a
+  print(P)
+
+
+  # 8. Circumference of a circle
+  PI=3.14
+  r=4
+  C=2*PI*r
+  print(C)
+
+
+  # 9. Swapping of two numbers
+  x,y=10,20
+  temp=x
+  x=y
+  y=temp
+  print(x,y)
+
+  x=input('Enter value of x: ')
+  y=input('Enter value of y: ')
+  x,y=y,x
+  print(x)
+  print(y)
